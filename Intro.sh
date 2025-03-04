006bc076f6e71f736eca09ecb4c20ce86aac2433
anas20023 <anasibnebelal400@gmail.com>
2025-03-04 11:43:19 +0600

    Refactor Intro.sh to include comparison logic, case statements, and a factorial calculation

--- Intro.sh
+++ Intro.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
-read -r x
-read -r y
-echo $(($x + $y))
+# read -r x
+# read -r y
+# if(( x>y ));
+# then 
+# echo "First one is greater"
+# elif((x==y));
+# then
+# echo "Both are equal"
+# else
+# echo "Second one is greater"
+# fi
+
+# shellcheck disable=SC2034
+# shellcheck disable=SC2162
+# read -p "Enter a Fruit Name :" fr
+
+# case $fr in
+# "Apple") echo "Apple";;
+# "Mango") echo "Mango";;
+# *) echo "Others";;
+# esac 
+
+# counter=10
+
+# for((i=1;i<=5;i++))
+# do
+# tm=$counter
+# tm=$((tm*i))
+#  counter=$tm
+#  #echo $i
+# done
+
+# echo $counter
+
+
